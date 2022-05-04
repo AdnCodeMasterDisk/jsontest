6052f56cbdb409bb05aff65cdd98bc4cb993a4a6
adncode <deepartesdigitales@gmail.com>
2022-05-04 12:27:31 -0500

    Create items.php

---
+++ items.php
@@ -0,0 +1,33 @@
+{
+  "status": "true",
+  "message": "Data fetched successfully!",
+  "data": [
+    {
+      "title": "Title item 1",
+      "subtittle": "Subtitle item 1",
+      "image" : "https://storage.googleapis.com/gweb-uniblog-publish-prod/images/GMP.max-200x200.png",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt "
+    },
+
+    {
+      "title": "Title item 2",
+      "subtittle": "Subtitle item 2",
+      "image" : "https://www.internetmatters.org/wp-content/uploads/2018/01/Twitter-logo-1-300x300-200x200.png",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt "
+    },
+    
+    {
+      "title": "Title item 3",
+      "subtittle": "Subtitle item 2",
+      "image" : "https://www.internetmatters.org/wp-content/uploads/2018/01/Twitter-logo-1-300x300-200x200.png",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt "
+    },
+    
+     {
+      "title": "Title item 4",
+      "subtittle": "Subtitle item 2",
+      "image" : "https://www.internetmatters.org/wp-content/uploads/2018/01/Twitter-logo-1-300x300-200x200.png",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt "
+    }
+  ]
+}
